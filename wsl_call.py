369d03d78e935caa904e449287176e6e079a112d
Edmar D. O. Filho <87383421+Edmar8111@users.noreply.github.com>
2025-09-23 03:01:09 -0400

    Add files via upload

---
+++ wsl_call.py
@@ -0,0 +1,43 @@
+import subprocess
+import sys
+
+class SubPro:
+  def __init__(self, install=None, uninstall=None,id_service=None):
+    try:
+      if install==True and uninstall==None:
+        pacote_arg=f'wls --install'
+      elif uninstall==True and install==None:
+        pacote_arg=f'dism.exe /online /disable-feature /featurename:Microsoft-Windows-Subsystem-Linux /norestart'
+    except ValueError as e:
+      return f'Erro value -> {e}'  
+    try:
+      resultado=subprocess.run(pacote_arg,check=True,capture_output=True,text=True)
+      return f'Pacote instalado com sucesso\n {resultado.stdout}'
+    except subprocess.CalledProcessError as erro:
+      print(f'ERRO AO TENTAR INSTALAR PACOTE -> {erro.returncode} \n{erro.stderr}')
+      sys.exit(1)
+    return
+  def remover_distro(self, distro_name):
+    pacote_arg=f'wls --unregister {distro_name}'
+    try:
+      resultado=subprocess.run(pacote_arg,check=True,capture_output=True,text=True)
+      return f'Pacote removido com sucesso\n {resultado.stdout}'
+    except subprocess.CalledProcessError as erro:
+      print(f'ERRO AO TENTAR REMOVER OS -> {erro.returncode} \n{erro.stderr}')
+      sys.exit(1)
+    return
+  def listar_distros(self):
+    pacote_arg=f'wls --list --verbose'
+    try:
+      resultado=subprocess.run(pacote_arg,check=True,capture_output=True,text=True)
+      return f'DISTROS INSTALADAS: \n {resultado.stdout}'
+    except subprocess.CalledProcessError as erro:
+      print(f'ERRO AO TENTAR RETORNAR DISTROS -> {erro.returncode} \n{erro.stderr}')
+      sys.exit(1)
+    return
+    
+
+if __name__=='__main__':
+  print("🚀 WSL process...")
+  SubPro('arg')
+  print('Sera necessario o reinicio da maquina!')
